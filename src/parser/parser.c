982be06e563577c344773a079f78c60d0a009fc4
Julian <92728359+Julian1612@users.noreply.github.com>
2023-01-31 17:07:09 +0100

    added macros to the c files and some commands to the bashfile

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,23 +6,17 @@
 /*   By: jschneid <jschneid@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 19:14:00 by jschneid          #+#    #+#             */
-/*   Updated: 2023/01/31 14:55:06 by jschneid         ###   ########.fr       */
+/*   Updated: 2023/01/31 17:03:20 by jschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 #include <stdio.h>
 
-int parser(int *argc, char **argv)
+int parser(int *argc, char **argv, t_map *map_data)
 {
-	(void)argv;
 	(void)argc;
-	if (*argc > 2 || *argc < 2)
-	{
-		printf("error");
-		return (0);
-	}
-	else
-		printf("correct");
-	return (0);
+	(void)argv;
+	(void)map_data;
+	return (1);
 }
