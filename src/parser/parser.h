f7299a1807245e830dd48c4b1da409ee47c2ab8a
Julian <92728359+Julian1612@users.noreply.github.com>
2023-01-31 15:04:52 +0100

    started with the parser

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -1,25 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   parser.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jschneid <jschneid@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/23 15:28:59 by jschneid          #+#    #+#             */
-/*   Updated: 2023/01/30 20:23:16 by jschneid         ###   ########.fr       */
+/*   Created: 2023/01/30 19:13:15 by jschneid          #+#    #+#             */
+/*   Updated: 2023/01/30 20:22:16 by jschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../includes/cub3D.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <memory.h>
-#define WIDTH 256
-#define HEIGHT 256
+#ifndef PARSER_H
+# define PARSER_H
 
-int	main(int argc, char **argv)
-{
-	parser(&argc, argv);
-	return (0);
-}
+# include "./../../includes/cub3D.h"
+
+int parser(int *argc, char **argv);
+
+#endif
