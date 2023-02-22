92018d09d4aa5369f5ebf1f0cc1f5462f95e3e37
Julian <92728359+Julian1612@users.noreply.github.com>
2023-02-22 15:06:57 +0100

    improved norm

--- src/parser/error_messages.c
+++ src/parser/error_messages.c
@@ -6,7 +6,7 @@
 /*   By: jschneid <jschneid@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 15:03:15 by jschneid          #+#    #+#             */
-/*   Updated: 2023/02/22 14:32:09 by jschneid         ###   ########.fr       */
+/*   Updated: 2023/02/22 15:06:09 by jschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,26 +23,4 @@ void	error_message(int error_code)
 		printf("Error!\nFile has wrong datatype\n");
 	else if (error_code == 4)
 		printf("Error!\nMalloc failed\n");
-	else if (error_code == 5)
-		printf("Error!\nMultiple texture definitions\n");
-	else if (error_code == 6)
-		printf("Error!\nMap is not surrunded by walls\n");
-	else if (error_code == 7)
-		printf("Error!\nFailed getting path of texture\n");
-	else if (error_code == 8)
-		printf("Error!\nFile is empty\n");
-	else if (error_code == 9)
-		printf("Error!\nfile contains no map\n");
-	else if (error_code == 10)
-		printf("Error!\nFilepath to .xpm file is missing\n");
-	else if (error_code == 13)
-		printf("Error!\nMap is in incorrect position. Please place the map under the texture declarations\n");
-	else if (error_code == 14)
-		printf("Error!\nThe values for the RGBs are missing\n");
-	else if (error_code == 15)
-		printf("Error!\nEmty line in map\n");
-	else if (error_code == 16)
-		printf("Error!\nInvalid character in map\n");
-	else if (error_code == 17)
-		printf("Error!\nRGB value out of range\n");
 }
