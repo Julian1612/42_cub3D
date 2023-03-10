f3eaf90850353d7a88f1963e9c69054f294b58c2
Julian <92728359+Julian1612@users.noreply.github.com>
2023-03-10 09:47:58 +0100

    started with the restructure off the get path function

--- src/parser/get_textures/get_path.c
+++ src/parser/get_textures/get_path.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_path.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jschneid <jschneid@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jschneid <jschneid@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 13:46:22 by jschneid          #+#    #+#             */
-/*   Updated: 2023/03/09 17:49:56 by jschneid         ###   ########.fr       */
+/*   Updated: 2023/03/09 21:18:28 by jschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,103 @@
 int			find_line(char *cub_file_path, char *direction, char **line_content);
 static int	get_texture_file_path(char **texture_path, char *line_content);
 
+static int	get_texture_file_path(char **texture_path, char *line_content)
+{
+	char **splitted_str;
+
+	splitted_str = ft_split(line_content, ' ');
+	*texture_path = malloc(sizeof(char) * ft_strlen(splitted_str[1]));
+	if (*texture_path == NULL)
+	{
+		error_textures(4);
+		return (1);
+	}
+	// checken of split 2 elemente hat
+	// error returnen
+	if (cpy_line(texture_path, splitted_str[1], ft_strlen(&line_content[1]) - 1) == NULL)
+		return (1);
+	return (0);
+}
+`
 // quasi ne while die einmal durch geht vergelicht strstr ob in der line eine definition da, wenn ja dann
 // rein laden in struct.
-// flag die gesetzt wird wenn die map gefunde wurde, wenn dann noch eine weitere texture definition gefunden wird
-// dann error
-// am ende gucken ob die essentials da sind, wenn nicht dann erro
 // die get rgb funktion auch hier direkt mit machen das mach nicht nochmal durber eiern muss
 // einfach if c oder f dann rgb values rein schallern
 // wenn er auf den anfang der map stößt dann eine funktion die die map in ein array schreibt und einfach bis zum ende rein lad wenn dann
 // noche eine definition drin ist muss das spater beim map checken abgefangen werden
+int check_for_texture(char *line, char **texture_path)
+{
+
+	// macro definieren der alle sachen enthalt (we, ea, so etc.) und den dann in einer while durch gehen und
+	// vergleichen ob verhanden wie unten schon gemacht nor halt das es ein varibaler macro is in dem man nyr ein mal anpassen
+	// falls noch eine neue texture hinzu kommt
+	char *definitions[];
+
+	definitions = ["WE", "EA", "SO", "NO"]
+	// die bonus texturen noch da zu !!!
+	if (ft_strstr(line, "WE", ft_strlen(line)) != NULL)
+	{
+		get_texture_file_path(texture_path, line);
+		return (0)
+	}
+	else if (ft_strstr(line, "EA", ft_strlen(line)) != NULL)
+	{
+		get_texture_file_path(texture_path, line);
+		return (0)
+	}
+	else if (ft_strstr(line, "SO", ft_strlen(line)) != NULL)
+	{
+		get_texture_file_path(texture_path, line);
+		return (0)
+	}
+	else if (ft_strstr(line, "NO", ft_strlen(line)) != NULL)
+	{
+		get_texture_file_path(texture_path, line);
+		return (0)
+	}
+	return (0);
+}
+
+int check_line(char **texture_path, char *line)
+{
+	// in der line is eine texture definition return 0
+	if(check_for_texture(line, texture_path))
+		return (0);
+	else if (check_fo)
+	// else if in der line is eine rgb definition return 0
+	// else if in der line beginnt die map return 1
+	return (0);
+}
+
 int	get_path(char *direction, char **texture_path, char *cub_file_path)
 {
 	int		i;
-	char	*line_content;
+	char	*line;
+	int		file_len;
+	int		fd;
 
 	i = 0;
-	line_content = NULL;
-	if (find_line(cub_file_path, direction, &line_content))
+	line = NULL;
+	fd = open(cub_file_path, O_RDONLY);
+	if (fd == -1)
+	{
+		error_message(2);
 		return (1);
-	if (get_texture_file_path(texture_path, line_content))
+	}
+	while (i < file_len - 1)
+	{
+		line = get_next_line(fd);
+		if (line == NULL && i != file_len - 1)
+		{
+			error_message(4);
+			return (1);
+		}
+		if(check_line(line, texture_path))
+			break ;
+		free(line);
+		i++;
+	}
+	if (get_texture_file_path(texture_path, line))
 		return (1);
 	return (0);
 }
@@ -99,23 +177,6 @@ int	find_line(char *cub_file_path, char *direction, char **line_content)
 	return (0);
 }
 
-static int	get_texture_file_path(char **texture_path, char *line_content)
-{
-	char **splitted_str;
-
-	splitted_str = ft_split(line_content, ' ');
-	*texture_path = malloc(sizeof(char) * ft_strlen(splitted_str[1]));
-	if (*texture_path == NULL)
-	{
-		error_textures(4);
-		return (1);
-	}
-	// checken of split 2 elemente hat
-	// error returnen
-	if (cpy_line(texture_path, splitted_str[1], ft_strlen(&line_content[1]) - 1) == NULL)
-		return (1);
-	return (0);
-}
 
 // static int	get_data_texture(char *raw_line, char **path)
 // {
