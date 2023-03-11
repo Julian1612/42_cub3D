2e4dbf1b5c476684c6c83a2f17d190e64b19c66c
Julian Schneider <jtschneider99@icloud.com>
2023-03-11 13:13:49 +0100

    added the soundtrack

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jschneid <jschneid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 15:28:59 by jschneid          #+#    #+#             */
-/*   Updated: 2023/02/27 18:57:38 by jschneid         ###   ########.fr       */
+/*   Updated: 2023/03/11 11:51:22 by jschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,49 +87,15 @@ int draw_walls(mlx_t *mlx, t_map *map_data)
 	return 0;
 }
 
+void	play_music(void)
+{
+	system("afplay ./preussengloria.mp3 &");
+}
+
+
 int	main(int argc, char **argv)
 {
-	mlx_t			*mlx;
-	t_player		player;
-	t_hook			hook_data;
-	t_map			map_data;
-	// t_player		player_pos;
-	int i = 0;
-	int j = 0;
-
 	(void)argc;
 	(void)argv;
-	if (parser(&argc, argv, &map_data))
-		return (1);
-	if (!(mlx = mlx_init ((map_data.map_hight * WALL_BLOCK),(map_data.map_width * WALL_BLOCK)-WALL_BLOCK, "Cub3D", true)))
-		return (EXIT_FAILURE);
-	player.player_angle = M_PI;
-	hook_data.mlx = mlx;
-	hook_data.player = &player;
-	// if (init_player_position(&map_data, &player_pos))
-	// 	return (1);
-	img = mlx_new_image(mlx, PLAYER_SIZE, PLAYER_SIZE);
-	view_direction = mlx_new_image(mlx, DIRECTION, DIRECTION);
-	walls = mlx_new_image(mlx, WALL_BLOCK, WALL_BLOCK);
-	memset(img->pixels, 255, img->width * img->height * sizeof(int));
-	memset(view_direction->pixels, 255, view_direction->width * view_direction->height * sizeof(int));
-	memset(walls->pixels, 255, walls->width * walls->height * sizeof(int));
-	while ((i * WALL_BLOCK) < (map_data.map_hight * WALL_BLOCK))
-	{
-		while ((j * WALL_BLOCK) < (map_data.map_width * WALL_BLOCK))
-		{
-			if (map_data.map[i][j] == '1')
-				mlx_image_to_window(mlx, walls, i * WALL_BLOCK, j * WALL_BLOCK);
-			j++;
-		}
-		j = 0;
-		i++;
-	}
-	mlx_image_to_window(mlx, img, 60, 80);
-	mlx_image_to_window(mlx, view_direction, 0, 0);
-	mlx_loop_hook(mlx, &hook, &hook_data);
-	mlx_loop(mlx);
-	mlx_terminate(mlx);
-	return (0);
-
+	play_music();
 }
