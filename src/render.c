66bad067db4a8e449d4dbb3c8c0f577e2c2f9cd4
lorbke <luca.orbke@brinkjost.de>
2023-03-19 17:27:58 +0100

    removed unnecessary function

--- src/render.c
+++ src/render.c
@@ -6,7 +6,7 @@
 /*   By: lorbke <lorbke@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 13:46:52 by lorbke            #+#    #+#             */
-/*   Updated: 2023/03/19 17:26:37 by lorbke           ###   ########.fr       */
+/*   Updated: 2023/03/19 17:27:38 by lorbke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@ int	render_minimap(t_minimap *minimap, mlx_t *mlx, t_map *map)
 
 	i = 0;
 	j = 0;
-	// @todo replace with actual map width and height
 	while ((i * MM_BLOCK_SIZE) < (map->height * MM_BLOCK_SIZE))
 	{
 		while ((j * MM_BLOCK_SIZE) < (map->width * MM_BLOCK_SIZE))
@@ -46,44 +45,6 @@ int	render_minimap(t_minimap *minimap, mlx_t *mlx, t_map *map)
 		return (ERROR);
 	return (SUCCESS);
 }
-
-// int	render_minimap(t_minimap *minimap, mlx_t *mlx, char **map)
-// {
-// 	int	i;
-// 	int	j;
-// 	int	map_width;
-// 	int	map_height;
-
-// 	i = 0;
-// 	j = 0;
-// 	// @todo replace with actual map width and height
-// 	map_width = 16;
-// 	map_height = 9;
-// 	while ((i * MM_BLOCK_SIZE) < (map_height * MM_BLOCK_SIZE))
-// 	{
-// 		while ((j * MM_BLOCK_SIZE) < (map_width * MM_BLOCK_SIZE))
-// 		{
-// 			if (i % 2 == 0 && j % 2 == 0)
-// 			{
-// 				if (mlx_image_to_window(mlx, minimap->walls, i * MM_BLOCK_SIZE, j * MM_BLOCK_SIZE) == ERROR)
-// 					return (ERROR);
-// 			}
-// 			if (i % 2 != 0 && j % 2 != 0)
-// 			{
-// 				if (mlx_image_to_window(mlx, minimap->walls, i * MM_BLOCK_SIZE, j * MM_BLOCK_SIZE) == ERROR)
-// 					return (ERROR);
-// 			}
-// 			j++;
-// 		}
-// 		j = 0;
-// 		i++;
-// 	}
-// 	if (mlx_image_to_window(mlx, minimap->player, 0, 0) == ERROR)
-// 		return (ERROR);
-// 	if (mlx_image_to_window(mlx, minimap->view_dir, 0, 0) == ERROR)
-// 		return (ERROR);
-// 	return (SUCCESS);
-// }
 
 void	color_image(mlx_t *mlx, mlx_image_t *img, unsigned int color)
 {
