16757ce9035a2451703c17da69b1f86314efd1e5
lorbke <luca.orbke@brinkjost.de>
2023-03-26 16:56:03 +0200

    switched rotation direction

--- src/hook.c
+++ src/hook.c
@@ -6,7 +6,7 @@
 /*   By: lorbke <lorbke@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 18:51:13 by lorbke            #+#    #+#             */
-/*   Updated: 2023/03/26 00:09:31 by lorbke           ###   ########.fr       */
+/*   Updated: 2023/03/26 16:55:29 by lorbke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,9 +81,9 @@ void	keys(mlx_t *mlx, t_minimap *minimap, t_player *player, char **map)
 		}
 	}
 	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
-		player->view_dir += M_PI / 90 / ROT_SPEED; // @note radian rotated by 5 degrees (1pi = 180 degrees)
-	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
 		player->view_dir -= M_PI / 90 / ROT_SPEED;
+	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
+		player->view_dir += M_PI / 90 / ROT_SPEED; // @note radian rotated by 5 degrees (1pi = 180 degrees)
 	if (mlx_is_key_down(mlx, MLX_KEY_1))
 		player->view_dir = 0;
 	if (mlx_is_key_down(mlx, MLX_KEY_2))
