6fb19801eb09a5001ffac0c7f1be0066600c2c97
lorbke <luca.orbke@brinkjost.de>
2023-04-02 18:46:02 +0200

    remove merge commits

---
+++ libraries/mlx/build/CMakeFiles/mlx42.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/lorbke/Coding/42_cub3D/libraries/mlx/lib/glad/glad.c" "CMakeFiles/mlx42.dir/lib/glad/glad.c.o" "gcc" "CMakeFiles/mlx42.dir/lib/glad/glad.c.o.d"
+  "/Users/lorbke/Coding/42_cub3D/libraries/mlx/lib/png/lodepng.c" "CMakeFiles/mlx42.dir/lib/png/lodepng.c.o" "gcc" "CMakeFiles/mlx42.dir/lib/png/lodepng.c.o.d"
+  "/Users/lorbke/Coding/42_cub3D/libraries/mlx/build/mlx_frag_shader.c" "CMakeFiles/mlx42.dir/mlx_frag_shader.c.o" "gcc" "CMakeFiles/mlx42.dir/mlx_frag_shader.c.o.d"
+  "/Users/lorbke/Coding/42_cub3D/libraries/mlx/build/mlx_vert_shader.c" "CMakeFiles/mlx42.dir/mlx_vert_shader.c.o" "gcc" "CMakeFiles/mlx42.dir/mlx_vert_shader.c.o.d"
+  "/Users/lorbke/Coding/42_cub3D/libraries/mlx/src/font/mlx_font.c" "CMakeFiles/mlx42.dir/src/font/mlx_font.c.o" "gcc" "CMakeFiles/mlx42.dir/src/font/mlx_font.c.o.d"
+  "/Users/lorbke/Coding/42_cub3D/libraries/mlx/src/mlx_cursor.c" "CMakeFiles/mlx42.dir/src/mlx_cursor.c.o" "gcc" "CMakeFiles/mlx42.dir/src/mlx_cursor.c.o.d"
+  "/Users/lorbke/Coding/42_cub3D/libraries/mlx/src/mlx_exit.c" "CMakeFiles/mlx42.dir/src/mlx_exit.c.o" "gcc" "CMakeFiles/mlx42.dir/src/mlx_exit.c.o.d"
+  "/Users/lorbke/Coding/42_cub3D/libraries/mlx/src/mlx_images.c" "CMakeFiles/mlx42.dir/src/mlx_images.c.o" "gcc" "CMakeFiles/mlx42.dir/src/mlx_images.c.o.d"
+  "/Users/lorbke/Coding/42_cub3D/libraries/mlx/src/mlx_init.c" "CMakeFiles/mlx42.dir/src/mlx_init.c.o" "gcc" "CMakeFiles/mlx42.dir/src/mlx_init.c.o.d"
+  "/Users/lorbke/Coding/42_cub3D/libraries/mlx/src/mlx_keys.c" "CMakeFiles/mlx42.dir/src/mlx_keys.c.o" "gcc" "CMakeFiles/mlx42.dir/src/mlx_keys.c.o.d"
+  "/Users/lorbke/Coding/42_cub3D/libraries/mlx/src/mlx_loop.c" "CMakeFiles/mlx42.dir/src/mlx_loop.c.o" "gcc" "CMakeFiles/mlx42.dir/src/mlx_loop.c.o.d"
+  "/Users/lorbke/Coding/42_cub3D/libraries/mlx/src/mlx_monitor.c" "CMakeFiles/mlx42.dir/src/mlx_monitor.c.o" "gcc" "CMakeFiles/mlx42.dir/src/mlx_monitor.c.o.d"
+  "/Users/lorbke/Coding/42_cub3D/libraries/mlx/src/mlx_mouse.c" "CMakeFiles/mlx42.dir/src/mlx_mouse.c.o" "gcc" "CMakeFiles/mlx42.dir/src/mlx_mouse.c.o.d"
+  "/Users/lorbke/Coding/42_cub3D/libraries/mlx/src/mlx_put_pixel.c" "CMakeFiles/mlx42.dir/src/mlx_put_pixel.c.o" "gcc" "CMakeFiles/mlx42.dir/src/mlx_put_pixel.c.o.d"
+  "/Users/lorbke/Coding/42_cub3D/libraries/mlx/src/mlx_window.c" "CMakeFiles/mlx42.dir/src/mlx_window.c.o" "gcc" "CMakeFiles/mlx42.dir/src/mlx_window.c.o.d"
+  "/Users/lorbke/Coding/42_cub3D/libraries/mlx/src/textures/mlx_png.c" "CMakeFiles/mlx42.dir/src/textures/mlx_png.c.o" "gcc" "CMakeFiles/mlx42.dir/src/textures/mlx_png.c.o.d"
+  "/Users/lorbke/Coding/42_cub3D/libraries/mlx/src/textures/mlx_texture.c" "CMakeFiles/mlx42.dir/src/textures/mlx_texture.c.o" "gcc" "CMakeFiles/mlx42.dir/src/textures/mlx_texture.c.o.d"
+  "/Users/lorbke/Coding/42_cub3D/libraries/mlx/src/textures/mlx_xpm42.c" "CMakeFiles/mlx42.dir/src/textures/mlx_xpm42.c.o" "gcc" "CMakeFiles/mlx42.dir/src/textures/mlx_xpm42.c.o.d"
+  "/Users/lorbke/Coding/42_cub3D/libraries/mlx/src/utils/mlx_compare.c" "CMakeFiles/mlx42.dir/src/utils/mlx_compare.c.o" "gcc" "CMakeFiles/mlx42.dir/src/utils/mlx_compare.c.o.d"
+  "/Users/lorbke/Coding/42_cub3D/libraries/mlx/src/utils/mlx_error.c" "CMakeFiles/mlx42.dir/src/utils/mlx_error.c.o" "gcc" "CMakeFiles/mlx42.dir/src/utils/mlx_error.c.o.d"
+  "/Users/lorbke/Coding/42_cub3D/libraries/mlx/src/utils/mlx_list.c" "CMakeFiles/mlx42.dir/src/utils/mlx_list.c.o" "gcc" "CMakeFiles/mlx42.dir/src/utils/mlx_list.c.o.d"
+  "/Users/lorbke/Coding/42_cub3D/libraries/mlx/src/utils/mlx_utils.c" "CMakeFiles/mlx42.dir/src/utils/mlx_utils.c.o" "gcc" "CMakeFiles/mlx42.dir/src/utils/mlx_utils.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
