588eb4d5f41a46f400274dbc7c72ebf7941383b4
lorbke <luca.orbke@brinkjost.de>
2023-04-12 19:14:53 +0200

    prepare merge

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jschneid <jschneid@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: lorbke <lorbke@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 15:28:59 by jschneid          #+#    #+#             */
-/*   Updated: 2023/04/12 16:58:14 by jschneid         ###   ########.fr       */
+/*   Updated: 2023/04/12 19:12:28 by lorbke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,66 +52,23 @@ int	main(int argc, char **argv)
 
 	if (parser(&argc, argv, &game))
 	{
-		// system("leaks cub3D");
 		return (EXIT_FAILURE);
 	}
-	// if (initialize_mlx_data(&game) == ERROR)
-	// 	errexit_mlx_errno();
-	// if (mlx_loop_hook(game.mlx, &hook, &game) == false)
-	// 	errexit_mlx_errno();
-	// if (mlx_image_to_window(game.mlx, game.img_world, 0, 0) == ERROR)
-	// 	errexit_mlx_errno();
-	// if (mlx_image_to_window(game.mlx, game.img_hud, 0, 0) == ERROR)
-	// 	errexit_mlx_errno();
-	// if (initialize_minimap(&game) == ERROR)
-	// 	errexit_mlx_errno();
-	// if (initialize_start_screen(&game) == ERROR)
-	// 	errexit_mlx_errno();
-	// draw_background(&game.start_screen);
-	// mlx_loop(game.mlx);
-	// mlx_loop(game.mlx);
-	// mlx_terminate(game.mlx);
-	int i = 0;
-	while (i < 4)
-	{
-		printf("%s\n", game.map.textures[i]->path);
-		i++;
-	}
-	i = 0;
-	printf("--------------------------------------------\n");
-	printf("--------------------------------------------\n");
-	printf("game.map.enemy_count = %d\n", game.map.enemy_count);
-	printf("--------------------------------------------\n");
-	printf("--------------------------------------------\n");
-	while (i < game.map.enemy_count)
-	{
-		printf("--------------------------------------------\n");
-		printf("game.map.enemies[i].alive = %d\n", game.map.enemies[i].alive);
-		printf("game.map.enemies[i].pos.x = %f\n", game.map.enemies[i].pos.x);
-		printf("game.map.enemies[i].pos.y = %f\n", game.map.enemies[i].pos.y);
-		printf("game.map.enemies[i].health = %d\n", game.map.enemies[i].health);
-		printf("game.map.enemies[i].last_frame_time = %f\n", game.map.enemies[i].last_frame_time);
-		printf("game.map.enemies[i].curr_frame = %d\n", game.map.enemies[i].curr_frame);
-		printf("game.map.enemies[i].speed = %f\n", game.map.enemies[i].speed);
-		printf("game.map.enemies[i].damage = %d\n", game.map.enemies[i].damage);
-		i++;
-	}
-	i = 0;
-	printf("--------------------------------------------\n");
-	printf("--------------------------------------------\n");
-	printf("game.map.door_count = %d\n", game.map.door_count);
-	printf("--------------------------------------------\n");
-	printf("--------------------------------------------\n");
-	while (i < game.map.door_count)
-	{
-		printf("--------------------------------------------\n");
-		printf("game->map.doors[count_doors].x = %d\n", game.map.doors[i].x);
-		printf("game->map.doors[count_doors].y = %d\n", game.map.doors[i].y);
-		printf("game->map.doors[count_doors].open = %d\n", game.map.doors[i].open);
-		printf("game->map.doors[count_doors].last_action = %f\n", game.map.doors[i].last_action);
-		i++;
-	}
-	printf("42 cub3D by jschneid and jhauk\n");
+	if (initialize_mlx_data(&game) == ERROR)
+		errexit_mlx_errno();
+	if (mlx_loop_hook(game.mlx, &hook, &game) == false)
+		errexit_mlx_errno();
+	if (mlx_image_to_window(game.mlx, game.img_world, 0, 0) == ERROR)
+		errexit_mlx_errno();
+	if (mlx_image_to_window(game.mlx, game.img_hud, 0, 0) == ERROR)
+		errexit_mlx_errno();
+	if (initialize_minimap(&game) == ERROR)
+		errexit_mlx_errno();
+	if (initialize_start_screen(&game) == ERROR)
+		errexit_mlx_errno();
+	draw_background(&game.start_screen);
+	mlx_loop(game.mlx);
+	mlx_terminate(game.mlx);
 	// free_data(&game.map);
 	return (EXIT_SUCCESS);
 }
